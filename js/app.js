a3283289e38bedab23c2d38a5c1fea70b700f5d5
kyle8285 <kylelussier3@gmail.com>
2015-04-07 12:06:03 -0400

    working on CSS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$(".buttons").hide();
-$(".buttons").show('slow');
+// $(".buttons").hide();
+// $(".buttons").show('slow');
 
 
 //when 'Submit Your Guess' button is pressed
@@ -8,11 +8,28 @@ $("input").submit(function(){
 	alert("You hit the ENTER button!");
 });
 
+// $("#submitGuess").on('click', function() {
+// 	alert("You clicked the submit button.");
+// });
+
 $(".buttons").on('click', function() {
+	// if ($(this) === $("#submitGuess") {
+	// 	alert("You clicked the submit button.");
+	// } 
 	alert("You clicked a button.");
 });
 
 $("input").on('keyup', function() {
 	var number = $(this).val();
 });
+
+//when a game begins(page loads or 'Play Again' clicked, there should be a random 
+//number generated between 1 and 100
+
+	//returns a random integer between min (included) and max (excluded). 
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
+	};
+
+
 
